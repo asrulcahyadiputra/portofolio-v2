4ccbe6072474e55a14cd9987bd39f183e2067e76
asrulcahyadiputra <asrulcahyadiputra99@gmail.com>
2022-12-07 01:51:44 +0800

    update rel no reopener

--- pages/index.js
+++ pages/index.js
@@ -50,13 +50,13 @@ export default function Home() {
                   </p>
 
                   <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
-                        <a href="https://www.instagram.com/asrulcahyadi99/" target="_blank" rel="noopener noreferrer">
+                        <a href="https://www.instagram.com/asrulcahyadi99/" target="_blank" rel="noreferrer">
                             <AiFillInstagram/>
                         </a>
-                          <a href="https://www.linkedin.com/in/asrul-cahyadi-putra/" target="_blank" rel="noopener noreferrer">
+                          <a href="https://www.linkedin.com/in/asrul-cahyadi-putra/" target="_blank" rel="noreferrer">
                               <AiFillLinkedin/>
                           </a>
-                          <a href="https://github.com/asrulcahyadiputra" target="_blank" rel="noopener noreferrer">
+                          <a href="https://github.com/asrulcahyadiputra" target="_blank" rel="noreferrer">
                               <AiFillGithub/>
                           </a>
                   </div>
